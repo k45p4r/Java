9246ff6f3decfa280b84c9fa1652b6db8b3f4a9a
k45p4r <kasparv@metropolia.fi>
2024-01-22 16:48:05 +0200

    push

---
+++ src/Task4.java
@@ -0,0 +1,24 @@
+public class Task4 {
+private final String name;
+
+public Task4(String name) {
+    this.name = name;
+}
+
+public void meow() {
+    System.out.println("The cat named " + name + " says: Meow!");
+}
+
+public static void main(String[] args) {
+    // Create an instance of the Cat class with a name
+    Task4 cat = new Task4("Whiskers");
+
+    // Call the meow method on the cat instance
+    cat.meow();
+    cat.meow();
+    cat = new Task4("Rex");
+    cat.meow();
+    cat = new Task4("Whiskers");
+    cat.meow();
+    }
+}
