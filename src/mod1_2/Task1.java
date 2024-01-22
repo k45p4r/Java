371bf7c03ba717e9bda5e57ae40b2ee0526486de
k45p4r <kasparv@metropolia.fi>
2024-01-22 23:28:34 +0200

    push

--- src/mod1_2/Task1.java
+++ src/mod1_2/Task1.java
@@ -8,5 +8,6 @@ public class Task1 {
         float fahr = Float.parseFloat(scanner.nextLine());
         float cels = ((fahr-32)*5/9);
         System.out.printf(fahr + " Fahrenheits is " +  "%.1f Celsius.", cels);
+        scanner.close();
     }
 }
